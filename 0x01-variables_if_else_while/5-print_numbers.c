8eb9885da477f0e6f575d5890707fc4c329c72ab
Emmanuel <sobowalegz2@gmail.com>
2023-02-17 16:17:16 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all numbers between 0 and 10
+ * Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		print("%d\n", number;
+	print('\n');
+	return (0);
+}
