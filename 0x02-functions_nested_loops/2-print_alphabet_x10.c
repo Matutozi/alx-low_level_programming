4bbb80351295471168e2524cda0c25634bc6741a
Emmanuel <sobowalegz2@gmail.com>
2023-02-21 12:03:47 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - to print all the alphabets 10 times
+ * Return: 0 when successful
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count = 1;
+
+	while (count < 11)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		putchar('\n');
+	}
+}
