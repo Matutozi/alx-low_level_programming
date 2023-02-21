213c783d44ddccc44388f1d04bad0360dfac2789
Emmanuel <sobowalegz2@gmail.com>
2023-02-21 11:51:41 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - to list all the alphabets in small letters
+ * Return: return O when sucessful
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
