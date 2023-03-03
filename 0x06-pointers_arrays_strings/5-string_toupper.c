98658664d81c5f7d802f6b70dff95eb8b18910b2
Emmanuel <sobowalegz2@gmail.com>
2023-03-03 11:59:18 -0800

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - to convert all lowercase letters to uppercase
+ * @n: parameter to consider
+ *
+ * Return: return n
+*/
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'a')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
