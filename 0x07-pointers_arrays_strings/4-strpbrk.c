7a49dc0a1902c232a8628a23531e4f111f3b712d
Emmanuel <sobowalegz2@gmail.com>
2023-03-06 13:07:56 -0800

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - to search through two strings for commin strings
+ * @s: first parameter
+ * @accept: second parameter to consider
+ * Return: returns a pointer to a byte is s;
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; i <= accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return s;
+		}
+	s++;
+	}
+	return ('\0');
+}
