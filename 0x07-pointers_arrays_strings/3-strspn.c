d6df7b5cac11fb1df39fc35271c9b6fc1aabbbeb
Emmanuel <sobowalegz2@gmail.com>
2023-03-06 12:58:44 -0800

    3- strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - functon that gets the length of a string
+ * @s: first parameter
+ * @accept: second parameter
+ *
+ * Return: returns the number of times it finds the element in a given string
+ *
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter;
+	int i;
+
+	i = 0;
+	counter = 0;
+	while (*s)
+	{
+		for (; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				counter++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (counter);
+		}
+		s++;
+	}
+	return (counter);
+}
