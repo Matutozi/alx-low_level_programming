7a36ffaefd5f6d34509f02cbe14782345bdfe2a8
Emmanuel <sobowalegz2@gmail.com>
2023-03-07 11:32:07 -0800

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,6 @@ char *_strstr(char *haystack, char *needle)
 		char *i = haystack;
 		char *p = needle;
 
-		haystack++;
 		while (*i == *p && *p != '\0')
 		{
 			i++;
@@ -25,6 +24,7 @@ char *_strstr(char *haystack, char *needle)
 
 		if (*p == '\0')
 			return (haystack);
+		haystack++;
 	}
 	return (0);
 }
