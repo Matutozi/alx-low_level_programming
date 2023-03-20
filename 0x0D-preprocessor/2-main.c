fa22158d74743b2806290d8e873c46859b5beb55
Emmanuel <sobowalegz2@gmail.com>
2023-03-20 12:42:01 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - to write a functio that prints the name of the file
+ * Return: returns void
+ * Description: a functon that gives the file
+ *
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
