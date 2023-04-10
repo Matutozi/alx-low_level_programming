5dd7f8792f4d1dff0719f038f59d93e2dc1ef7aa
Emmanuel <sobowalegz2@gmail.com>
2023-04-09 17:58:55 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		fp2 = write(fp1, text_content, strlen(text_content));
+		fp2 = write(fp1, text_content, sizeof(text_content));
 		if (fp2 == -1)
 			return (-1);
 	}
