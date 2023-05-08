c557670c35340223876794d8c9d202997478b980
Matutozi <sobowalegz2@gmail.com>
2023-05-08 16:36:11 -0700

    main function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-
+char *create_buffer(char *file);
+void close_file(int fd);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
