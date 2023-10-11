e92d6b0c0c0e551d4a36b802227fb1f853be782c
Matutozi <sobowalegz2@gmail.com>
2023-10-11 05:08:56 +0100

    minor adjustment to the documentation and betty style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,45 @@
-#include <stdio.h>
-#include <stddef.h>
 #include "search_algos.h"
+
 /**
- * binary_search - binary search algorithm
+ * binary_search - function that searches for values using binary search
  * @array: pointer to first element of array
  * @size: size of array
  * @value: value to search for
- * Return: value index, or -1 if not found or code fail
- */
+ * Return: returns value index, or -1 if not successful
+*/
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, l_idx, r_idx;
+	size_t i, l_index r_index;
 	int mid;
 
 	if (array == NULL)
 		return (-1);
 
-	l_idx = 0;
-	r_idx = size - 1;
+	l_index = 0;
+	r_index = size - 1;
 
-	while (l_idx <= r_idx)
+	while (l_index <= r_index)
 	{
 		printf("Searching in array: ");
 
-		for (i = l_idx; i <= r_idx; i++)
+		for (i = l_index; i <= r_index; i++)
 		{
 			printf("%d", array[i]);
-			if (i != r_idx)
+			if (i != r_index)
 				printf(", ");
 			else
 				printf("\n");
 		}
 
-		mid = (l_idx + r_idx) / 2;
+		mid = (l_index + r_index) / 2;
 
 		if (array[mid] > value)
-			r_idx = mid - 1;
+			r_index = mid - 1;
 		else if (array[mid] < value)
-			l_idx = mid + 1;
+			l_index = mid + 1;
 		else
 			return (mid);
 	}
 	return (-1);
-}#include "search_algos.h"
-
-/**
-* 
+}
